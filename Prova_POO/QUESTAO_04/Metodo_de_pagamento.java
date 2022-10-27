f80e1ab521caa2e00962b42859386039367a4fbf
404notfound-max <60528618+404notfound-max@users.noreply.github.com>
2022-10-26 21:29:19 -0300

    Update Metodo_de_pagamento.java

--- Prova_POO/QUESTAO_04/Metodo_de_pagamento.java
+++ Prova_POO/QUESTAO_04/Metodo_de_pagamento.java
@@ -1,5 +1,6 @@
-package POO.Prova_POO.QUESTAO_04;
+package ProvaQ04;
 
-public interface Metodo_de_pagamento {
-    public String processar();
+public interface MetodoDePagamento 
+{
+    public String Pagamento();
 }
